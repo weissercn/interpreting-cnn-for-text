74fe590ef7ffd725ba1757c1af136dcea6db54b5
Constantin Weisser <weisser@mit.edu>
2020-04-30 18:33:01 -0400

    tested whole ensemble and split up between true negatives, true positives ...

--- MIT_adversarial.py
+++ MIT_adversarial.py
@@ -13,20 +13,33 @@ def eval_adversary(model, data, config):
     model.eval()
     n_iter = 0
     #epoch_x, epoch_y, lengths_x = get_epoch(data["valid_x"], data["valid_y"], config["batch_size"], is_train=False)
-    epoch_x, epoch_y, lengths_x = get_epoch(data["valid_x"], data["valid_y"], 1, is_train=False, num_examples=1)
+    epoch_x, epoch_y, lengths_x = get_epoch(data["valid_x"], data["valid_y"], 1, is_train=False) #num_examples=1)
     epoch_loss = 0
     corrects = 0
     criterion = nn.CrossEntropyLoss()
 
-    advers_attack = [data["word_to_idx"][a] for a in ["surreal", "heart"]]
+    advers_attack = {
+        "TP": [],
+        "TN": [data["word_to_idx"][a] for a in ["surreal", "heart"]],
+        "FP": [],
+        "FN": [data["word_to_idx"][a] for a in ["surreal", "heart"]],
+        }
 
+
+    results = {    
+        "TP": [0,0], # number of examples changed and unchanged by adversarial attack
+        "TN": [0,0],
+        "FP": [0,0],
+        "FN": [0,0], 
+        }
 
     for batch_x, batch_y, length_x in zip(epoch_x, epoch_y, lengths_x):
-        batch_x_advers = [a+advers_attack for a in batch_x ]
-        length_x_advers = [a+len(advers_attack) for a in  length_x]
+        #batch_x_advers = [a+advers_attack for a in batch_x ]
+        #length_x_advers = [a+len(advers_attack) for a in  length_x]
         
         batch_x_orig = batch_x.copy()
-        batch_x_advers_orig = batch_x_advers.copy()
+        length_x_orig = length_x.copy()
+        #batch_x_advers_orig = batch_x_advers.copy()
 
         batch_x = torch.LongTensor(batch_x)
         batch_y = torch.LongTensor(batch_y)
@@ -44,10 +57,20 @@ def eval_adversary(model, data, config):
             batch_y = batch_y.cpu().detach().numpy()
 
         #this only works if batch size is 1
-        if pred_class[0]==0 and batch_y[0]==0: # Adversarial attack on negative samples. If the model correctly predicts that a sample belongs to class 0. 
 
-            batch_x_advers = torch.LongTensor(batch_x_advers)
-            lengths_x_advers = torch.LongTensor(length_x_advers)
+        TYPE = ""
+        if pred_class[0]==1 and batch_y[0]==1: TYPE = "TP"
+        elif pred_class[0]==0 and batch_y[0]==0: TYPE = "TN"
+        elif pred_class[0]==0 and batch_y[0]==1: TYPE = "FP"
+        elif pred_class[0]==1 and batch_y[0]==0: TYPE = "FN"
+
+        if TYPE=="TN": # Adversarial attack on negative samples. If the model correctly predicts that a sample belongs to class 0. 
+            # TN 
+            batch_x_advers_orig = [a+advers_attack[TYPE] for a in batch_x_orig ]
+            length_x_advers_orig = [a+len(advers_attack[TYPE]) for a in  length_x_orig]
+            
+            batch_x_advers = torch.LongTensor(batch_x_advers_orig)
+            lengths_x_advers = torch.LongTensor(length_x_advers_orig)
 
             if config["cuda"]:
                 batch_x_advers, lengths_x_advers = batch_x_advers.cuda(), lengths_x_advers.cuda()
@@ -58,26 +81,18 @@ def eval_adversary(model, data, config):
             if config["cuda"]:
                 pred_class_advers = pred_class_advers.cpu().detach().numpy()
 
-            #print(data["idx_to_word"].keys(), len(data["idx_to_word"].keys()))
-            print("Original sentence : ", " ".join([data["idx_to_word"][a] for a in batch_x_orig[0]  ]))
-            print("Adversarial sentence : ", " ".join([data["idx_to_word"][a] for a in batch_x_advers_orig[0]  ]))
-            print("Truth {}, Pred {}, Adversarial {}".format(batch_y[0],pred_class[0],pred_class_advers[0] ))
+            if pred_class[0] == pred_class_advers[0]: results[TYPE][1]+=1
+            else: results[TYPE][0]+=1
 
-        """
-        loss = criterion(pred, batch_y)
-        n_iter += 1
-        epoch_loss += float(loss)
+            if False:
+                #print(data["idx_to_word"].keys(), len(data["idx_to_word"].keys()))
+                print("Original sentence : ", " ".join([data["idx_to_word"][a] for a in batch_x_orig[0]  ]))
+                print("Adversarial sentence : ", " ".join([data["idx_to_word"][a] for a in batch_x_advers_orig[0]  ]))
+                print("Truth {}, Pred {}, Adversarial {}".format(batch_y[0],pred_class[0],pred_class_advers[0] ))
 
-        batch_corrects = int((torch.max(pred, 1)[1].view(batch_y.size()).data == batch_y.data).sum())
-        corrects += batch_corrects
-        """
-        # if n_iter % 200 == 0:
-        #     eval()
-        #     model.train()
-        
-        #del batch_x, batch_y, pred, loss
 
-    #return epoch_loss / len(data["valid_y"]), corrects / len(data["valid_y"]) * 100
+    print("results : ", results)        
+
 
 
 
